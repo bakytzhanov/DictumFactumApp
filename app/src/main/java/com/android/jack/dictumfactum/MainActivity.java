c382252be9e526be02b6030b9b05ad7e9928cf05
Batyrbek <bakhytzhanov.b.k@gmail.com>
2019-01-06 23:59:57 +0600

    fragment chats

--- app/src/main/java/com/android/jack/dictumfactum/MainActivity.java
+++ app/src/main/java/com/android/jack/dictumfactum/MainActivity.java
@@ -3,11 +3,17 @@ package com.android.jack.dictumfactum;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.android.jack.dictumfactum.Model.User;
@@ -19,6 +25,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -63,6 +71,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        TableLayout tableLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+
     }
 
 
@@ -89,4 +106,44 @@ public class MainActivity extends AppCompatActivity {
         return false;
 
     }
+
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
+
+
 }
