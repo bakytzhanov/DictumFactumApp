00cb7543e7a01ea4d7089b78b7df8e577a1f3a77
batyr <bakhytzhanov.b.k@gmail.com>
2019-02-08 17:49:14 +0600

    post image AddPostActivity and ProfileFragment

--- app/src/main/java/com/android/jack/dictumfactum/AddPostActivity.java
+++ app/src/main/java/com/android/jack/dictumfactum/AddPostActivity.java
@@ -41,12 +41,22 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddPostActivity extends AppCompatActivity {
 
+    CircleImageView post_image;
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+
+
     EditText header_post;
     EditText field_post;
     Button add_one_post;
 
-
     DatabaseReference reference;
+
+    final String newsid = UUID.randomUUID().toString();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,10 +69,12 @@ public class AddPostActivity extends AppCompatActivity {
         toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
 
 
+        post_image = findViewById(R.id.post_image);
+
+
         header_post = findViewById(R.id.header_post);
         field_post = findViewById(R.id.field_post);
         add_one_post = findViewById(R.id.add_one_post);
-
 
 
         add_one_post.setOnClickListener(new View.OnClickListener() {
@@ -70,8 +82,9 @@ public class AddPostActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String txt_header = header_post.getText().toString();
                 String txt_field = field_post.getText().toString();
-                String newsid = UUID.randomUUID().toString();
+
                 reference = FirebaseDatabase.getInstance().getReference("News").child(newsid);
+
 
 
                 reference.child("id").setValue(newsid);
@@ -82,6 +95,99 @@ public class AddPostActivity extends AppCompatActivity {
 
             }
         });
+
+
+        post_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
+
+    }
+
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+    }
+
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = AddPostActivity.this.getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(AddPostActivity.this);
+        pd.setMessage("Загружается");
+        pd.show();
+
+
+        if(imageUri != null){
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    +"."+getFileExtension(imageUri));
+
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot,Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+
+                    if(!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+
+                    if(task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("News").child(newsid);
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL", mUri);
+                        reference.updateChildren(map);
+
+                        pd.dismiss();
+                    }else{
+                        Toast.makeText(AddPostActivity.this, "Ошибка", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(AddPostActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+        }else{
+            Toast.makeText(AddPostActivity.this, "Не выбран рисунок", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode == IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+
+
+            if(uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(AddPostActivity.this, "Загрузка", Toast.LENGTH_SHORT).show();
+            }else{
+                uploadImage();
+            }
+        }
 
     }
 }
